24b19b39830cddf0e3543d44818df19960c1732e
patriciaacpd <patriciacpd9@gmail.com>
2023-05-23 16:04:41 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	let [status,setStatus] = useState("desactivado");
+	let [rojo,setRojo] = useState("desactivado");
+	let [amarillo,setAmarillo] = useState("desactivado");
+	let [verde,setVerde] = useState("desactivado");
+
+
+	const handleStatus = (statusParametro) => {
+		
+
+		if (statusParametro==="activadorojo"){
+			setStatus (statusParametro)
+		}
+		else if (statusParametro==="activadoamarillo"){
+			setStatus (statusParametro)
+		}
+		else if (statusParametro==="activadoverde"){
+			setStatus (statusParametro)
+		}
+		
+
+	};
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container d-flex flex-column justify-content-start align-items-center sectionall">
+			<div className="bg-black w-5 top"><p></p></div>
+			<div className="divgeneralnegro bg-black d-flex flex-column mybox rounded">
+				<button onClick= {() => handleStatus("activadorojo")} type="button" className={`btn btn-danger rounded-circle my-2 mx-2  ${status  === "activadorojo"  ? "light" : ""}`}><p></p></button>
+				<button onClick={() => handleStatus("activadoamarillo")} type="button" className={`btn btn-warning rounded-circle my-2 mx-2 ${status  === "activadoamarillo" ? "light" : ""}`}><p></p></button>
+				<button onClick={() => handleStatus("activadoverde")} type="button" className={`btn btn-success rounded-circle my-2 mx-2 ${status  === "activadoverde" ? "light" : ""}`}><p></p></button>
+
+			</div>
 		</div>
+
+
+
 	);
 };
 
